11c65ac9b07393c7edea8234c14e7537f8248ec8
Arham Islam <arham.m.islam@gmail.com>
2024-03-29 17:46:54 -0400

    Registering an account & hashed passwords
    - register.php completed where one can register for an account
    - passwords are now hashed

--- functions.php
+++ functions.php
@@ -1,15 +1,15 @@
 <!--
     Author: Arham Islam
-    File Description: PHP functions for myfitness.
     Date Created: March 02, 2024
+    File Description: PHP file that contains all the functions.
 -->
 
 <?php
     require("database.php");
 
     /*
-    * @desc function to get all the users in the database
-    * @return array of users
+    * @desc function to get the all users in the database
+    * @return array $users
     */
 
     function getUsers() {
@@ -23,20 +23,68 @@
     /*
     * @desc function to validate login information
     * @param string $email, string $password
-    * @return boolean validLogin
+    * @return boolean
     */
 
-    function isValidLogin($email, $password) {
+    function isValidLogin($emailInput, $passwordInput) {
         $users = getUsers();
 
+        // Loop through users array and check if the user inputted email matches one of the email's in the database,
+        // then check if hashed password in the database is the same as the inputted password.
+        // If both match, then return true. Otherwise, return false.
         foreach($users as $u) {
-            if($u["user_email"] == $email) {
-                if($u["user_pass"] == $password) {
+            if($u["user_email"] == $emailInput) {
+                if(password_verify($passwordInput, $u["user_pass"])) {
                     return true;
                 }
             }
         }
 
         return false;
+    }
+
+    /*
+    * @desc function to create a new user
+    * @param string $fName, string $lName, string $email, string $password
+    * @return boolean
+    */
+
+    function createUser($fName, $lName, $email, $password) {
+        $users = getUsers();
+
+        // Loop through users array and check if user inputted email matches one of the email's in the database,
+        // if it does, then return false
+        foreach($users as $u) {
+            if($u["user_email"] == $email) {
+                return false;
+            }
+        }
+
+        // Making sure new user does not make an account with null
+        if($fName == "null" || $lName == "null" || $email == "null" || $password == "null") {
+            return false;
+        }
+
+        // Making sure new user does not make an account with null
+        if($fName == "NULL" || $lName == "NULL" || $email == "NULL" || $password == "NULL") {
+            return false;
+        }
+
+        // Making sure new user does not make an account with empty input fields
+        if($fName == "" || $lName == "" || $email == "" || $password == "") {
+            return false;
+        }
+
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+        global $pdo;
+        $query = $pdo -> prepare("INSERT INTO users (user_fname, user_lname, user_email, user_pass) VALUES (:fname, :lname, :email, :pass)");
+        $query -> bindparam(":fname", $fName);
+        $query -> bindparam(":lname", $lName);
+        $query -> bindparam(":email", $email);
+        $query -> bindparam(":pass", $hashedPassword);
+        $query -> execute();
+
+        return true;
     }
 ?>
--- register.php
+++ register.php
@@ -1,8 +1,25 @@
 <!--
     Author: Arham Islam
-    File Description: Registering for an account page.
     Date Created: February 26, 2024
+    File Description: PHP file for users to register an account.
 -->
+
+<?php
+    require("functions.php");
+
+    //
+    if(isset($_POST["fName"]) && isset($_POST["lName"]) && isset($_POST["email"]) && isset($_POST["password"])) {
+        $newUser = createUser($_POST["fName"], $_POST["lName"], $_POST["email"], $_POST["password"]);
+
+        if($newUser) {
+            echo "SUCCESS";
+        }
+
+        else {
+            echo "FAILURE";
+        }
+    }
+?>
 
 <!doctype html>
 <html lang="en">
@@ -15,9 +32,50 @@
 
         <link rel="stylesheet" href="styles.css">
         <link rel="icon" href="pictures/favicon.ico">
-        <title>myfitness</title>
+        <title>myfitness | Register</title>
     </head>
     <body>
+        <div class="container-fluid">
+            <div class="row align-items-center text-center logo">
+                <div class="col-md p-0">
+                    <span class="text logoMy">my</span><span class="text logoFitness">fitness</span>
+                    <p class="text logoShortStrapline">Own Your Fitness Journey</p>
+                </div>
+                <div class="col-md p-0">
+                    <div class="rounded-5 shadow mx-auto p-4 loginBox">
+                        <p class="h2 mb-4 fw-bold">Sign Up</p>
+
+                        <form action="" method="POST">
+                            <div class="mb-3 text-start">
+                                <div class="row g-3">
+                                    <div class="col">
+                                        <label for="firstName">First name</label>
+                                        <input type="firstName" class="form-control" aria-label="First name" name="fName">
+                                    </div>
+                                    <div class="col">
+                                        <label for="lastName">Last name</label>
+                                        <input type="lastName" class="form-control" aria-label="Last name" name="lName">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="mb-3 text-start">
+                                <label for="email">Email address</label>
+                                <input type="email" class="form-control" id="email" name="email">
+                            </div>
+                            <div class="mb-4 text-start">
+                                <label for="password">Password</label>
+                                <input type="password" class="form-control" id="password" name="password">
+                            </div>
+
+                            <input type="submit" class="btn btn-dark mb-3 w-100 fw-bold" value="Sign Up"></input>
+                            <p>Already have an account? <a href="login.php" class="text-decoration-none fw-bold signUp">Sign In</a></p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <br><br><br><br>
+            <p class="small text-center">© 2024 myfitness. All Rights Reserved.</p>
+        </div>
 
         <!-- jQuery & Bootstrap -->
         <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
